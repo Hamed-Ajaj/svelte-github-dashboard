41deb829416e03228d91c3f777674587c2919a5b
hamed ajaj <hamedsgaming@gmail.com>
2025-07-27 17:27:18 +0300

    Revert "removing secrets"
    This reverts commit 6b7938556209ea775182a55df7307e4006eab457.

--- src/routes/repos/[user]/+page.js
+++ src/routes/repos/[user]/+page.js
@@ -1,12 +1,8 @@
 export async function load({ params }) {
 
-  const token = import.meta.env.VITE_GITHUB_TOKEN;
-  
   const { user } = params
 
-  const repos = fetch(`https://api.github.com/users/${user}/repos`, {
-    headers: { Authorization: `token ${token}` }
-  })
+  const repos = fetch(`https://api.github.com/users/${user}/repos`)
     .then(res => res.json())
     .catch(error => console.log(error))
 

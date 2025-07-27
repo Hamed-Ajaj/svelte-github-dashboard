6b7938556209ea775182a55df7307e4006eab457
hamed ajaj <hamedsgaming@gmail.com>
2025-07-27 16:12:34 +0300

    removing secrets

--- src/routes/repos/[user]/+page.js
+++ src/routes/repos/[user]/+page.js
@@ -1,8 +1,12 @@
 export async function load({ params }) {
 
+  const token = import.meta.env.VITE_GITHUB_TOKEN;
+  
   const { user } = params
 
-  const repos = fetch(`https://api.github.com/users/${user}/repos`)
+  const repos = fetch(`https://api.github.com/users/${user}/repos`, {
+    headers: { Authorization: `token ${token}` }
+  })
     .then(res => res.json())
     .catch(error => console.log(error))
 
